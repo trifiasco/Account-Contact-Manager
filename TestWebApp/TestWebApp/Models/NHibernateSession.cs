e751164b72866b96fa4d18a2d652be2b41d7b125
ENOSISBD\arnabroy <trifiasco@gmail.com>
2017-12-04 17:04:51 +0600

    Added a class to open session for NHibernate

---
+++ TestWebApp/TestWebApp/Models/NHibernateSession.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NHibernate;
+using NHibernate.Cfg;
+
+namespace TestWebApp.Models
+{
+    public class NHibernateSession
+    {
+        public static ISession OpenSession()
+        {
+            var configuration = new Configuration();
+            var configurationPath = HttpContext.Current.Server.MapPath(@"~\NhibernateFolder\hibernate.cfg.xml");
+            configuration.Configure(configurationPath);
+            var employeeConfigurationFile = HttpContext.Current.Server.MapPath(@"~\NhibernateFolder\Account.hbm.xml");
+            configuration.AddFile(employeeConfigurationFile);
+            ISessionFactory sessionFactory = configuration.BuildSessionFactory();
+            return sessionFactory.OpenSession();
+        }
+    }
+}
