56dc659f5cbcdbcbf5fad9b71afc8a160440f162
ENOSISBD\arnabroy <trifiasco@gmail.com>
2017-12-05 18:44:46 +0600

    Added controller action for contact creation

--- TestWebApp/TestWebApp/Controllers/ContactController.cs
+++ TestWebApp/TestWebApp/Controllers/ContactController.cs
@@ -22,5 +22,44 @@ namespace TestWebApp.Controllers
                 return View(contact);
             }
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Home/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                Contact contact = new Contact();     //  Creating a new instance of the account
+                contact.Id = Convert.ToInt32(collection["Id"]);
+                contact.FirstName = collection["FirstName"].ToString();
+                contact.LastName = collection["LastName"].ToString();
+                contact.Email = collection["Email"].ToString();
+
+
+
+                // TODO: Add insert logic here
+                using (ISession session = NHibernateSession.OpenSessionForContact())
+                {
+                    using (ITransaction transaction = session.BeginTransaction())   //  Begin a transaction
+                    {
+                        session.Save(contact); //  Save the book in session
+                        transaction.Commit();   //  Commit the changes to the database
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                //ViewBag.Message = e.Message;
+                //e.Message;
+                return View();
+            }
+        }
+
     }
 }
