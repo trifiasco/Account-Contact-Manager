a8825edf53ed0383dfe6824879b72462ee742c1c
ENOSISBD\arnabroy <trifiasco@gmail.com>
2017-12-05 18:17:01 +0600

    Created an entity for Contact

---
+++ TestWebApp/TestWebApp/Entity/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestWebApp.Entity
+{
+    public class Contact
+    {
+        public virtual int Id { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual string Email { get; set; }
+    }
+}
