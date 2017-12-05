282711563901cdf90896b10f5715696ede246420
ENOSISBD\arnabroy <trifiasco@gmail.com>
2017-12-05 18:31:41 +0600

    Added Contact controller and associated Index action

---
+++ TestWebApp/TestWebApp/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NHibernate;
+using NHibernate.Linq;
+using TestWebApp.Models;
+using TestWebApp.Entity;
+
+namespace TestWebApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Your Contact page is here.";
+            using (ISession session = NHibernateSession.OpenSessionForContact())
+            {
+                var contact = session.Query<Contact>().ToList();
+                return View(contact);
+            }
+        }
+    }
+}
