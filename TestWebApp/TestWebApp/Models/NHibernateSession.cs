e442f7803669de31e8e3f75ca33733273f03717b
ENOSISBD\arnabroy <trifiasco@gmail.com>
2017-12-06 15:12:41 +0600

    changed open session functions for newly added nhibernate mapping.

--- TestWebApp/TestWebApp/Models/NHibernateSession.cs
+++ TestWebApp/TestWebApp/Models/NHibernateSession.cs
@@ -17,6 +17,8 @@ namespace TestWebApp.Models
             configuration.Configure(configurationPath);
             var accountConfigurationFile = HttpContext.Current.Server.MapPath(@"~\NhibernateFolder\Account.hbm.xml");
             configuration.AddFile(accountConfigurationFile);
+            var contactConfigurationFile = HttpContext.Current.Server.MapPath(@"~\NhibernateFolder\Contact.hbm.xml");
+            configuration.AddFile(contactConfigurationFile);
             ISessionFactory sessionFactory = configuration.BuildSessionFactory();
             return sessionFactory.OpenSession();
         }
@@ -26,8 +28,10 @@ namespace TestWebApp.Models
             var configuration = new Configuration();
             var configurationPath = HttpContext.Current.Server.MapPath(@"~\NhibernateFolder\hibernate.cfg.xml");
             configuration.Configure(configurationPath);
-            var accountConfigurationFile = HttpContext.Current.Server.MapPath(@"~\NhibernateFolder\Contact.hbm.xml");
+            var accountConfigurationFile = HttpContext.Current.Server.MapPath(@"~\NhibernateFolder\Account.hbm.xml");
             configuration.AddFile(accountConfigurationFile);
+            var contactConfigurationFile = HttpContext.Current.Server.MapPath(@"~\NhibernateFolder\Contact.hbm.xml");
+            configuration.AddFile(contactConfigurationFile);
             ISessionFactory sessionFactory = configuration.BuildSessionFactory();
             return sessionFactory.OpenSession();
         }
