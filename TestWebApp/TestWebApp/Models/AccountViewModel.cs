406f7ca20e70be905b4310b5b41988f2c35d8ff2
ENOSISBD\arnabroy <trifiasco@gmail.com>
2017-12-07 19:50:38 +0600

    Added a account view model

---
+++ TestWebApp/TestWebApp/Models/AccountViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TestWebApp.Entity;
+
+namespace TestWebApp.Models
+{
+    public class AccountViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public IList<Contact> Contacts { get; set; }
+    }
+}
