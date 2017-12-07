7437681f5622ab43cc6c6e7b768d85cfe292e47c
ENOSISBD\arnabroy <trifiasco@gmail.com>
2017-12-07 19:51:12 +0600

    Added a mapper method to map an account entity to account view model

---
+++ TestWebApp/TestWebApp/Helper/MapperForAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TestWebApp.Entity;
+using TestWebApp.Models;
+
+namespace TestWebApp.Helper
+{
+    public class MapperForAccount
+    {
+        public static List<AccountViewModel> MapToContactViewModel(List<Account> accounts)
+        {
+            var viewModelList = new List<AccountViewModel>();
+            foreach (var account in accounts)
+            {
+                viewModelList.Add(new AccountViewModel
+                {
+                    Id = account.Id,
+                    Name = account.Name,
+                    Contacts = account.Contacts
+                });
+
+            }
+
+            return viewModelList;
+        }
+    }
+}
