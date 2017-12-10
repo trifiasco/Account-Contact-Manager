8d39338a9d00bd8410414997cfc893d03ef0d9ca
ENOSISBD\arnabroy <trifiasco@gmail.com>
2017-12-10 11:49:05 +0600

    Added view model for account creation and edition

---
+++ TestWebApp/TestWebApp/Models/AccountEditViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TestWebApp.Entity;
+
+namespace TestWebApp.Models
+{
+    public class AccountEditViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ContactSelectId { get; set; }
+        public IList<Contact> Contacts { get; set; }
+    }
+}
