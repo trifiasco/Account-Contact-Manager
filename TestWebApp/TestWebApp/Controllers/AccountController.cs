9d13b5438bcca96cb3de182f48220071054f3761
ENOSISBD\arnabroy <trifiasco@gmail.com>
2017-12-12 17:50:03 +0600

    Removed Unnecessary spaces and viewbag message

--- TestWebApp/TestWebApp/Controllers/AccountController.cs
+++ TestWebApp/TestWebApp/Controllers/AccountController.cs
@@ -1,149 +1,140 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using NHibernate;
-using NHibernate.Linq;
-using TestWebApp.Models;
 using TestWebApp.Entity;
 using TestWebApp.Helper;
+using TestWebApp.Models;
 
 namespace TestWebApp.Controllers
 {
-    public class AccountController : Controller
+  public class AccountController : Controller
+  {
+    // GET: Account
+    public ActionResult Index()
     {
-        // GET: Account
-        public ActionResult Index()
-        {
-            ViewBag.Message = "Your account page is here.";
-
-                var accounts = AccountQueries.GetAllAccounts();
-                var viewModel = MapperForAccount.MapToAccountViewModel(accounts);
-                return View(viewModel);
-        }
-
-        public ActionResult Create()
-        {
-            Account account = new Account();
-            var contacts = ContactQueries.GetAllContacts();
-            account.Contacts = contacts;
-            var viewModel = MapperForAccount.MapToAccountCreateViewModel(account);
-            return View(viewModel);
-        }
-
-        // POST: Home/Create
-        [HttpPost]
-        public ActionResult Create(AccountCreateViewModel accountCreateViewModel)
-        {
-            try
-            {
-                Account account = new Account();
-                account.Id = accountCreateViewModel.Id;
-                account.Name = accountCreateViewModel.Name;
-                foreach (var contactSelectedId in accountCreateViewModel.ContactSelectId)
-                {
-                    var contact = ContactQueries.GetOneContact(contactSelectedId);
-                    account.Contacts.Add(contact);
-                }
-                
-                AccountQueries.InsertOneAccount(account);
-                return RedirectToAction("Index");
-            }
-            catch (Exception e)
-            {
-                return View();
-            }
-        }
-
-
-
-        public ActionResult Edit(int id)
-        {
-                var account = AccountQueries.GetOneAccount(id);
-                var contactSelectIds = new List<int>();
-                foreach(var contact in account.Contacts)
-                {
-                    contactSelectIds.Add(contact.Id);
-                }
-                
-                var contacts = ContactQueries.GetAllContacts();
-                account.Contacts = contacts;
-                var viewModel = MapperForAccount.MapToAccountEditViewModel(account,contactSelectIds);
-                return View(viewModel);
-
-        }
-
-
-        [HttpPost]
-        public ActionResult Edit(int id, AccountEditViewModel accountEditViewModel)
-        {
-            try
-            {
-
-                var accountUpdate = AccountQueries.GetOneAccount(id);
-
-                accountUpdate.Id = accountEditViewModel.Id;
-                accountUpdate.Name = accountEditViewModel.Name;
-                //check if one old contact id is not in the new selected contact id, then remove it
-                for (int i = 0; i < accountUpdate.Contacts.Count; i++)
-                {
-                    var contact = accountUpdate.Contacts[i];
-                    if (!accountEditViewModel.ContactSelectId.Contains(contact.Id))
-                    {
-                        accountUpdate.Contacts.Remove(contact);
-                    }
-                }
-
-                //check if one new contact is not in the old contact list, then add it
-                foreach (var contactId in accountEditViewModel.ContactSelectId)
-                {   
-                    var contact = ContactQueries.GetOneContact(contactId);
-                    if (accountUpdate.Contacts.Where(x=>x.Id==contact.Id).ToList().Count==0)
-                    {
-                        accountUpdate.Contacts.Add(contact);
-                    }
-                }
-
-                AccountQueries.InsertOneAccount(accountUpdate);
-                
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-
-        public ActionResult Details(int id)
-        {
-
-            var account = AccountQueries.GetOneAccount(id);
-            var viewModel = MapperForAccount.MapToAccountDetailsViewModel(account);
-            return View(viewModel);
-        }
-
-        public ActionResult Delete(int id)
-        {
-                var account = AccountQueries.GetOneAccount(id);
-                return View(account);
-        }
-
-
-        [HttpPost]
-        public ActionResult Delete(int id, Account account)
-        {
-            try
-            {
-                AccountQueries.DeleteOneAccount(account);
-                return RedirectToAction("Index");
-            }
-            catch (Exception exception)
-            {
-                return View();
-            }
-        }
-
+      var accounts = AccountQueries.GetAllAccounts();
+      var viewModel = MapperForAccount.MapToAccountViewModel(accounts);
+      return View(viewModel);
     }
+
+    public ActionResult Create()
+    {
+      Account account = new Account();
+      var contacts = ContactQueries.GetAllContacts();
+      account.Contacts = contacts;
+      var viewModel = MapperForAccount.MapToAccountCreateViewModel(account);
+      return View(viewModel);
+    }
+
+    // POST: Home/Create
+    [HttpPost]
+    public ActionResult Create(AccountCreateViewModel accountCreateViewModel)
+    {
+      try
+      {
+        Account account = new Account();
+        account.Id = accountCreateViewModel.Id;
+        account.Name = accountCreateViewModel.Name;
+        foreach (var contactSelectedId in accountCreateViewModel.ContactSelectId)
+        {
+          var contact = ContactQueries.GetOneContact(contactSelectedId);
+          account.Contacts.Add(contact);
+        }
+
+        AccountQueries.InsertOneAccount(account);
+        return RedirectToAction("Index");
+      }
+      catch (Exception e)
+      {
+        return View();
+      }
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var account = AccountQueries.GetOneAccount(id);
+      var contactSelectIds = new List<int>();
+      foreach (var contact in account.Contacts)
+      {
+        contactSelectIds.Add(contact.Id);
+      }
+
+      var contacts = ContactQueries.GetAllContacts();
+      account.Contacts = contacts;
+      var viewModel = MapperForAccount.MapToAccountEditViewModel(account, contactSelectIds);
+      return View(viewModel);
+    }
+
+
+    [HttpPost]
+    public ActionResult Edit(int id, AccountEditViewModel accountEditViewModel)
+    {
+      try
+      {
+        var accountUpdate = AccountQueries.GetOneAccount(id);
+
+        accountUpdate.Id = accountEditViewModel.Id;
+        accountUpdate.Name = accountEditViewModel.Name;
+        //check if one old contact id is not in the new selected contact id, then remove it
+        for (int i = 0; i < accountUpdate.Contacts.Count; i++)
+        {
+          var contact = accountUpdate.Contacts[i];
+          if (!accountEditViewModel.ContactSelectId.Contains(contact.Id))
+          {
+            accountUpdate.Contacts.Remove(contact);
+          }
+        }
+
+        //check if one new contact is not in the old contact list, then add it
+        foreach (var contactId in accountEditViewModel.ContactSelectId)
+        {
+          var contact = ContactQueries.GetOneContact(contactId);
+          if (accountUpdate.Contacts.Where(x => x.Id == contact.Id).ToList().Count == 0)
+          {
+            accountUpdate.Contacts.Add(contact);
+          }
+        }
+
+        AccountQueries.InsertOneAccount(accountUpdate);
+
+        return RedirectToAction("Index");
+      }
+      catch
+      {
+        return View();
+      }
+    }
+
+
+    public ActionResult Details(int id)
+    {
+
+      var account = AccountQueries.GetOneAccount(id);
+      var viewModel = MapperForAccount.MapToAccountDetailsViewModel(account);
+      return View(viewModel);
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var account = AccountQueries.GetOneAccount(id);
+      return View(account);
+    }
+
+
+    [HttpPost]
+    public ActionResult Delete(int id, Account account)
+    {
+      try
+      {
+        AccountQueries.DeleteOneAccount(account);
+        return RedirectToAction("Index");
+      }
+      catch (Exception)
+      {
+        return View();
+      }
+    }
+
+  }
 }
--- TestWebApp/TestWebApp/Controllers/ContactController.cs
+++ TestWebApp/TestWebApp/Controllers/ContactController.cs
@@ -11,132 +11,129 @@ using TestWebApp.Helper;
 
 namespace TestWebApp.Controllers
 {
-    public class ContactController : Controller
+  public class ContactController : Controller
+  {
+    // GET: Contact
+    public ActionResult Index()
     {
-        // GET: Contact
-        public ActionResult Index()
-        {
-            ViewBag.Message = "Your Contact page is here.";
-
-            var contact = ContactQueries.GetAllContacts();
-
-            var viewModel = Mapper.MapToContactViewModel(contact);
-            return View(viewModel);
-
-        }
-
-        public ActionResult Create()
-        {
-            // changes for associating dropdownlist- final
-
-            Contact contact = new Contact();
-
-            var accounts = AccountQueries.GetAllAccounts();
-            contact.Accounts = accounts;
-
-            var viewModel = Mapper.MapToContactCreateViewModel(contact);
-            return View(viewModel);
-        }
-
-        // POST: Contact/Create
-        [HttpPost]
-        public ActionResult Create(ContactCreateViewModel contactCreateViewModel)
-        {
-            try
-            {
-                Contact contact = new Contact();
-                contact.Id = contactCreateViewModel.Id;
-                contact.FirstName = contactCreateViewModel.FirstName;
-                contact.LastName = contactCreateViewModel.LastName;
-                contact.Email = contactCreateViewModel.Email;
-                
-
-                var account = AccountQueries.GetOneAccount(contactCreateViewModel.AccountOnSelect);
-                contact.Accounts.Add(account);
-
-                ContactQueries.InsertOneContact(contact);
-
-                return RedirectToAction("Index");
-            }
-            catch (Exception e)
-            {
-                ViewBag.Message = "Contact Creation Failed. Try another Email.";
-                //e.Message;
-                return View();
-            }
-        }
-
-        // GET: Contact/Edit
-        public ActionResult Edit(int id)
-        {
-                var contact = ContactQueries.GetOneContact(id);
-                var accounts = AccountQueries.GetAllAccounts();
-                contact.Accounts = accounts;
-
-                var viewModel = Mapper.MapToContactEditViewModel(contact);
-                return View(viewModel);
-
-        }
-
-        [HttpPost]
-        public ActionResult Edit(int id, ContactEditViewModel contactEditViewModel)
-        {
-            try
-            {
-
-                var contactUpdate = ContactQueries.GetOneContact(id);
-
-                contactUpdate.Id = contactEditViewModel.Id;
-                contactUpdate.FirstName = contactEditViewModel.FirstName;
-                contactUpdate.LastName = contactEditViewModel.LastName;
-                contactUpdate.Email = contactEditViewModel.Email;
-
-
-                var account = AccountQueries.GetOneAccount(contactEditViewModel.AccountOnSelect);
-                contactUpdate.Accounts.Add(account);
-
-
-                ContactQueries.InsertOneContact(contactUpdate);
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Contact/Details
-        public ActionResult Details(int id)
-        {
-            var contact = ContactQueries.GetOneContact(id);
-            var viewModel = Mapper.MapToContactDetailsViewModel(contact);
-            return View(viewModel);
-        }
-
-
-        //GET: Contact/Delete
-
-        public ActionResult Delete(int id)
-        {
-
-            var contact = ContactQueries.GetOneContact(id);
-            return View(contact);
-        }
-
-
-        [HttpPost]
-        public ActionResult Delete(int id, Contact contact)
-        {
-            try
-            {
-                ContactQueries.DeleteOneContact(contact);
-                return RedirectToAction("Index");
-            }
-            catch (Exception exception)
-            {
-                return View();
-            }
-        }
+      var contact = ContactQueries.GetAllContacts();
+      var viewModel = Mapper.MapToContactViewModel(contact);
+      return View(viewModel);
 
     }
+
+    public ActionResult Create()
+    {
+      // changes for associating dropdownlist- final
+
+      Contact contact = new Contact();
+
+      var accounts = AccountQueries.GetAllAccounts();
+      contact.Accounts = accounts;
+
+      var viewModel = Mapper.MapToContactCreateViewModel(contact);
+      return View(viewModel);
+    }
+
+    // POST: Contact/Create
+    [HttpPost]
+    public ActionResult Create(ContactCreateViewModel contactCreateViewModel)
+    {
+      try
+      {
+        Contact contact = new Contact();
+        contact.Id = contactCreateViewModel.Id;
+        contact.FirstName = contactCreateViewModel.FirstName;
+        contact.LastName = contactCreateViewModel.LastName;
+        contact.Email = contactCreateViewModel.Email;
+
+
+        var account = AccountQueries.GetOneAccount(contactCreateViewModel.AccountOnSelect);
+        contact.Accounts.Add(account);
+
+        ContactQueries.InsertOneContact(contact);
+
+        return RedirectToAction("Index");
+      }
+      catch (Exception e)
+      {
+        ViewBag.Message = "Contact Creation Failed. Try another Email.";
+        //e.Message;
+        return View();
+      }
+    }
+
+    // GET: Contact/Edit
+    public ActionResult Edit(int id)
+    {
+      var contact = ContactQueries.GetOneContact(id);
+      var accounts = AccountQueries.GetAllAccounts();
+      contact.Accounts = accounts;
+
+      var viewModel = Mapper.MapToContactEditViewModel(contact);
+      return View(viewModel);
+
+    }
+
+    [HttpPost]
+    public ActionResult Edit(int id, ContactEditViewModel contactEditViewModel)
+    {
+      try
+      {
+
+        var contactUpdate = ContactQueries.GetOneContact(id);
+
+        contactUpdate.Id = contactEditViewModel.Id;
+        contactUpdate.FirstName = contactEditViewModel.FirstName;
+        contactUpdate.LastName = contactEditViewModel.LastName;
+        contactUpdate.Email = contactEditViewModel.Email;
+
+
+        var account = AccountQueries.GetOneAccount(contactEditViewModel.AccountOnSelect);
+        contactUpdate.Accounts.Add(account);
+
+
+        ContactQueries.InsertOneContact(contactUpdate);
+        return RedirectToAction("Index");
+      }
+      catch
+      {
+        return View();
+      }
+    }
+
+    // GET: Contact/Details
+    public ActionResult Details(int id)
+    {
+      var contact = ContactQueries.GetOneContact(id);
+      var viewModel = Mapper.MapToContactDetailsViewModel(contact);
+      return View(viewModel);
+    }
+
+
+    //GET: Contact/Delete
+
+    public ActionResult Delete(int id)
+    {
+
+      var contact = ContactQueries.GetOneContact(id);
+      return View(contact);
+    }
+
+
+    [HttpPost]
+    public ActionResult Delete(int id, Contact contact)
+    {
+      try
+      {
+        ContactQueries.DeleteOneContact(contact);
+        return RedirectToAction("Index");
+      }
+      catch (Exception exception)
+      {
+        return View();
+      }
+    }
+
+  }
 }
