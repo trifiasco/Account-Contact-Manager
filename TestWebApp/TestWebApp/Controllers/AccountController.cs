6a5c4168497320301585dc27e94e984ea9b7efa3
ENOSISBD\arnabroy <trifiasco@gmail.com>
2017-12-12 15:41:35 +0600

    Cleaned Account Controller using query wrapper class

--- TestWebApp/TestWebApp/Controllers/AccountController.cs
+++ TestWebApp/TestWebApp/Controllers/AccountController.cs
@@ -17,27 +17,19 @@ namespace TestWebApp.Controllers
         public ActionResult Index()
         {
             ViewBag.Message = "Your account page is here.";
-            using (ISession session = NHibernateSession.OpenSession())
-            {
-                var accounts = session.Query<Account>().ToList();
+
+                var accounts = AccountQueries.GetAllAccounts();
                 var viewModel = MapperForAccount.MapToAccountViewModel(accounts);
                 return View(viewModel);
-            }
         }
 
         public ActionResult Create()
         {
-            using (ISession session = NHibernateSession.OpenSession())
-            {
-                Account account = new Account();
-                using (ISession sessionGetContacts = NHibernateSession.OpenSessionForContact())
-                {
-                    var contacts = sessionGetContacts.Query<Contact>().ToList();
-                    account.Contacts = contacts;
-                }
-                var viewModel = MapperForAccount.MapToAccountCreateViewModel(account);
-                return View(viewModel);
-            }
+            Account account = new Account();
+            var contacts = ContactQueries.GetAllContacts();
+            account.Contacts = contacts;
+            var viewModel = MapperForAccount.MapToAccountCreateViewModel(account);
+            return View(viewModel);
         }
 
         // POST: Home/Create
@@ -49,31 +41,17 @@ namespace TestWebApp.Controllers
                 Account account = new Account();
                 account.Id = accountCreateViewModel.Id;
                 account.Name = accountCreateViewModel.Name;
-                using (ISession session = NHibernateSession.OpenSessionForContact())
+                foreach (var contactSelectedId in accountCreateViewModel.ContactSelectId)
                 {
-                    foreach (var contactSelectedId in accountCreateViewModel.ContactSelectId)
-                    {
-                        var contact = session.Get<Contact>(contactSelectedId);
-                        account.Contacts.Add(contact);
-                    }
-
+                    var contact = ContactQueries.GetOneContact(contactSelectedId);
+                    account.Contacts.Add(contact);
                 }
-
-                // TODO: Add insert logic here
-                using (ISession session = NHibernateSession.OpenSession())
-                {
-                    using (ITransaction transaction = session.BeginTransaction())   //  Begin a transaction
-                    {
-                        session.Save(account); //  Save the book in session
-                        transaction.Commit();   //  Commit the changes to the database
-                    }
-                }
+                
+                AccountQueries.InsertOneAccount(account);
                 return RedirectToAction("Index");
             }
             catch (Exception e)
             {
-                //ViewBag.Message = e.Message;
-                //e.Message;
                 return View();
             }
         }
@@ -82,24 +60,17 @@ namespace TestWebApp.Controllers
 
         public ActionResult Edit(int id)
         {
-            using (ISession session = NHibernateSession.OpenSession())
-            {
-                var account = session.Get<Account>(id);
+                var account = AccountQueries.GetOneAccount(id);
                 var contactSelectIds = new List<int>();
                 foreach(var contact in account.Contacts)
                 {
                     contactSelectIds.Add(contact.Id);
                 }
                 
-                using (ISession sessionGetContacts = NHibernateSession.OpenSessionForContact())
-                {
-                    var contacts = sessionGetContacts.Query<Contact>().ToList();
-                    account.Contacts = contacts;
-                }
-
+                var contacts = ContactQueries.GetAllContacts();
+                account.Contacts = contacts;
                 var viewModel = MapperForAccount.MapToAccountEditViewModel(account,contactSelectIds);
                 return View(viewModel);
-            }
 
         }
 
@@ -109,44 +80,33 @@ namespace TestWebApp.Controllers
         {
             try
             {
-                using (ISession session = NHibernateSession.OpenSession())
+
+                var accountUpdate = AccountQueries.GetOneAccount(id);
+
+                accountUpdate.Id = accountEditViewModel.Id;
+                accountUpdate.Name = accountEditViewModel.Name;
+                //check if one old contact id is not in the new selected contact id, then remove it
+                for (int i = 0; i < accountUpdate.Contacts.Count; i++)
                 {
-                    var accountUpdate = session.Get<Account>(id);
-
-                    accountUpdate.Id = accountEditViewModel.Id;
-                    accountUpdate.Name = accountEditViewModel.Name;
-                    //check if one old contact id is not in the new selected contact id, then remove it
-                    for (int i = 0; i < accountUpdate.Contacts.Count; i++)
+                    var contact = accountUpdate.Contacts[i];
+                    if (!accountEditViewModel.ContactSelectId.Contains(contact.Id))
                     {
-                        var contact = accountUpdate.Contacts[i];
-                        if (!accountEditViewModel.ContactSelectId.Contains(contact.Id))
-                        {
-                            accountUpdate.Contacts.Remove(contact);
-                        }
-                    }
-
-                    //check if one new contact is not in the old contact list, then add it
-                    foreach (var contactId in accountEditViewModel.ContactSelectId)
-                    {
-
-                        using (ISession sessionGetContact = NHibernateSession.OpenSessionForContact())
-                        {
-                            var contact = sessionGetContact.Get<Contact>(contactId);
-                            if (accountUpdate.Contacts.Where(x=>x.Id==contact.Id).ToList().Count==0)
-                            {
-                                accountUpdate.Contacts.Add(contact);
-                            }
-                        }
-                    }
-                    
-
-
-                    using (ITransaction transaction = session.BeginTransaction())
-                    {
-                        session.Save(accountUpdate);
-                        transaction.Commit();
+                        accountUpdate.Contacts.Remove(contact);
                     }
                 }
+
+                //check if one new contact is not in the old contact list, then add it
+                foreach (var contactId in accountEditViewModel.ContactSelectId)
+                {   
+                    var contact = ContactQueries.GetOneContact(contactId);
+                    if (accountUpdate.Contacts.Where(x=>x.Id==contact.Id).ToList().Count==0)
+                    {
+                        accountUpdate.Contacts.Add(contact);
+                    }
+                }
+
+                AccountQueries.InsertOneAccount(accountUpdate);
+                
                 return RedirectToAction("Index");
             }
             catch
@@ -158,21 +118,16 @@ namespace TestWebApp.Controllers
 
         public ActionResult Details(int id)
         {
-            using (ISession session = NHibernateSession.OpenSession())
-            {
-                var account = session.Get<Account>(id);
-                var viewModel = MapperForAccount.MapToAccountDetailsViewModel(account);
-                return View(viewModel);
-            }
+
+            var account = AccountQueries.GetOneAccount(id);
+            var viewModel = MapperForAccount.MapToAccountDetailsViewModel(account);
+            return View(viewModel);
         }
 
         public ActionResult Delete(int id)
         {
-            using (ISession session = NHibernateSession.OpenSession())
-            {
-                var account = session.Get<Account>(id);
+                var account = AccountQueries.GetOneAccount(id);
                 return View(account);
-            }
         }
 
 
@@ -181,14 +136,7 @@ namespace TestWebApp.Controllers
         {
             try
             {
-                using (ISession session = NHibernateSession.OpenSession())
-                {
-                    using (ITransaction transaction = session.BeginTransaction())
-                    {
-                        session.Delete(account);
-                        transaction.Commit();
-                    }
-                }
+                AccountQueries.DeleteOneAccount(account);
                 return RedirectToAction("Index");
             }
             catch (Exception exception)
