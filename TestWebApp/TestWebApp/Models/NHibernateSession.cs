6479843d229c0c19b13fbda58ad7c4fa9f2ac7ac
ENOSISBD\arnabroy <trifiasco@gmail.com>
2017-12-12 17:46:02 +0600

    Removed unnecessay method

--- TestWebApp/TestWebApp/Models/NHibernateSession.cs
+++ TestWebApp/TestWebApp/Models/NHibernateSession.cs
@@ -22,18 +22,5 @@ namespace TestWebApp.Models
             ISessionFactory sessionFactory = configuration.BuildSessionFactory();
             return sessionFactory.OpenSession();
         }
-
-        public static ISession OpenSessionForContact()
-        {
-            var configuration = new Configuration();
-            var configurationPath = HttpContext.Current.Server.MapPath(@"~\NhibernateFolder\hibernate.cfg.xml");
-            configuration.Configure(configurationPath);
-            var accountConfigurationFile = HttpContext.Current.Server.MapPath(@"~\NhibernateFolder\Account.hbm.xml");
-            configuration.AddFile(accountConfigurationFile);
-            var contactConfigurationFile = HttpContext.Current.Server.MapPath(@"~\NhibernateFolder\Contact.hbm.xml");
-            configuration.AddFile(contactConfigurationFile);
-            ISessionFactory sessionFactory = configuration.BuildSessionFactory();
-            return sessionFactory.OpenSession();
-        }
     }
 }
