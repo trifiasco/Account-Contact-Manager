376dd78ff3eb7f8f2049e3804f8e51b333fcfcc4
ENOSISBD\arnabroy <trifiasco@gmail.com>
2017-12-12 17:48:14 +0600

    Cleaned Code

--- TestWebApp/TestWebApp/Helper/MapperForAccount.cs
+++ TestWebApp/TestWebApp/Helper/MapperForAccount.cs
@@ -7,50 +7,50 @@ using TestWebApp.Models;
 
 namespace TestWebApp.Helper
 {
-    public class MapperForAccount
+  public class MapperForAccount
+  {
+    public static List<AccountViewModel> MapToAccountViewModel(List<Account> accounts)
     {
-        public static List<AccountViewModel> MapToAccountViewModel(List<Account> accounts)
+      var viewModelList = new List<AccountViewModel>();
+      foreach (var account in accounts)
+      {
+        viewModelList.Add(new AccountViewModel
         {
-            var viewModelList = new List<AccountViewModel>();
-            foreach (var account in accounts)
-            {
-                viewModelList.Add(new AccountViewModel
-                {
-                    Id = account.Id,
-                    Name = account.Name,
-                    Contacts = account.Contacts
-                });
+          Id = account.Id,
+          Name = account.Name,
+          Contacts = account.Contacts
+        });
 
-            }
+      }
 
-            return viewModelList;
-        }
-        public static AccountCreateViewModel MapToAccountCreateViewModel(Account account)
-        {
-            var accountViewModel = new AccountCreateViewModel();
-            accountViewModel.Id = account.Id;
-            accountViewModel.Name = account.Name;
-            accountViewModel.Contacts = account.Contacts;
-            return accountViewModel;
-        }
-
-        public static AccountEditViewModel MapToAccountEditViewModel(Account account,List<int>contactSelectIds)
-        {
-            var accountViewModel = new AccountEditViewModel();
-            accountViewModel.Id = account.Id;
-            accountViewModel.Name = account.Name;
-            accountViewModel.Contacts = account.Contacts;
-            accountViewModel.ContactSelectId = contactSelectIds;
-            return accountViewModel;
-        }
-
-        public static AccountDetailsViewModel MapToAccountDetailsViewModel(Account account)
-        {
-            var accountViewModel = new AccountDetailsViewModel();
-            accountViewModel.Id = account.Id;
-            accountViewModel.Name = account.Name;
-            accountViewModel.Contacts = account.Contacts;
-            return accountViewModel;
-        }
+      return viewModelList;
     }
+    public static AccountCreateViewModel MapToAccountCreateViewModel(Account account)
+    {
+      var accountViewModel = new AccountCreateViewModel();
+      accountViewModel.Id = account.Id;
+      accountViewModel.Name = account.Name;
+      accountViewModel.Contacts = account.Contacts;
+      return accountViewModel;
+    }
+
+    public static AccountEditViewModel MapToAccountEditViewModel(Account account, List<int> contactSelectIds)
+    {
+      var accountViewModel = new AccountEditViewModel();
+      accountViewModel.Id = account.Id;
+      accountViewModel.Name = account.Name;
+      accountViewModel.Contacts = account.Contacts;
+      accountViewModel.ContactSelectId = contactSelectIds;
+      return accountViewModel;
+    }
+
+    public static AccountDetailsViewModel MapToAccountDetailsViewModel(Account account)
+    {
+      var accountViewModel = new AccountDetailsViewModel();
+      accountViewModel.Id = account.Id;
+      accountViewModel.Name = account.Name;
+      accountViewModel.Contacts = account.Contacts;
+      return accountViewModel;
+    }
+  }
 }
