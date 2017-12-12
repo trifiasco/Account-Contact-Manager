10b39412f23befb3ccaba21488a7ec31435ed48e
ENOSISBD\arnabroy <trifiasco@gmail.com>
2017-12-12 11:55:08 +0600

    Created a R&D controller, model class and view to implement fully dynamic multiselect dropdownlist

---
+++ TestWebApp/TestWebApp/Models/RndModelView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TestWebApp.Entity;
+
+namespace TestWebApp.Models
+{
+    public class RndModelView
+    {
+        public RndModelView()
+        {
+            AlreadySelctedContacts = new List<Contact>();
+            NewContacts = new List<Contact>();
+            ContactSelectId = new List<int>();
+            ContactDiselectId = new List<int>();
+            ContactDummy1 = new List<Contact>();
+            ContactDummy2 = new List<Contact>();
+
+
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<int> ContactSelectId { get; set; }
+        public List<int> ContactDiselectId { get; set; }
+        public IList<Contact> AlreadySelctedContacts { get; set; }
+        public IList<Contact> NewContacts { get; set; }
+        public IList<Contact> ContactDummy1 { get; set; }
+        public IList<Contact> ContactDummy2 { get; set; }
+    }
+}
