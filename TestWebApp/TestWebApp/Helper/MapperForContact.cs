d53c36b00be49374efdf766828331c3a5106b132
ENOSISBD\arnabroy <trifiasco@gmail.com>
2017-12-13 15:12:08 +0600

    Updated mapper methods associated with auto mapping configuration

--- TestWebApp/TestWebApp/Helper/MapperForContact.cs
+++ TestWebApp/TestWebApp/Helper/MapperForContact.cs
@@ -4,71 +4,35 @@ using System.Linq;
 using System.Web;
 using TestWebApp.Entity;
 using TestWebApp.Models;
+using AutoMapper;
 
 namespace TestWebApp.Helper
 {
-    public static class MapperForContact
+  public static class MapperForContact
+  {
+    public static List<ContactViewModel> MapToContactViewModel(List<Contact> contacts)
     {
-        public static List<ContactViewModel> MapToContactViewModel(List<Contact> contacts)
-        {
-            var viewModelList = new List<ContactViewModel>();
-            foreach(var contact in contacts)
-            {
-                viewModelList.Add(new ContactViewModel {
-                    Id = contact.Id,
-                    FirstName = contact.FirstName,
-                    LastName = contact.LastName,
-                    Email = contact.Email,
-                    Accounts = contact.Accounts
-                });
+      var viewModelList = Mapper.Map<List<ContactViewModel>>(contacts);
+      return viewModelList;
+    }
 
-            }
+    public static ContactCreateViewModel MapToContactCreateViewModel(Contact contact)
+    {
+      var contactCreateViewModel = Mapper.Map<ContactCreateViewModel>(contact);
+      return contactCreateViewModel;
 
-            return viewModelList;
-        }
-
-        public static ContactCreateViewModel MapToContactCreateViewModel(Contact contact)
-        {
-            var contactCreateViewModel = new ContactCreateViewModel();
-            contactCreateViewModel.Id = contact.Id;
-            contactCreateViewModel.FirstName = contact.FirstName;
-            contactCreateViewModel.LastName = contact.LastName;
-            contactCreateViewModel.Email = contact.Email;
-
-            contactCreateViewModel.Accounts = contact.Accounts;
-
-            return contactCreateViewModel;
-
-
-        }
-        public static ContactEditViewModel MapToContactEditViewModel(Contact contact)
-        {
-            var contactEditViewModel = new ContactEditViewModel();
-            contactEditViewModel.Id = contact.Id;
-            contactEditViewModel.FirstName = contact.FirstName;
-            contactEditViewModel.LastName = contact.LastName;
-            contactEditViewModel.Email = contact.Email;
-
-            contactEditViewModel.Accounts = contact.Accounts;
-
-            return contactEditViewModel;
-
-
-        }
-        public static ContactDetailsViewModel MapToContactDetailsViewModel(Contact contact)
-        {
-            var contactDetailsViewModel = new ContactDetailsViewModel();
-            contactDetailsViewModel.Id = contact.Id;
-            contactDetailsViewModel.FirstName = contact.FirstName;
-            contactDetailsViewModel.LastName = contact.LastName;
-            contactDetailsViewModel.Email = contact.Email;
-
-            contactDetailsViewModel.Accounts = contact.Accounts;
-
-            return contactDetailsViewModel;
-
-
-        }
 
     }
+    public static ContactEditViewModel MapToContactEditViewModel(Contact contact)
+    {
+      var contactEditViewModel = Mapper.Map<ContactEditViewModel>(contact);
+      return contactEditViewModel;
+    }
+    public static ContactDetailsViewModel MapToContactDetailsViewModel(Contact contact)
+    {
+      var contactDetailsViewModel = Mapper.Map<ContactDetailsViewModel>(contact);
+      return contactDetailsViewModel;
+    }
+
+  }
 }
