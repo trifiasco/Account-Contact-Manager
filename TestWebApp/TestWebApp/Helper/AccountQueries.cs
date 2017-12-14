226058f854553b93948dec9b5ab78bbc8d2278a9
ENOSISBD\arnabroy <trifiasco@gmail.com>
2017-12-14 17:39:26 +0600

    Added criteria query for custom view

--- TestWebApp/TestWebApp/Helper/AccountQueries.cs
+++ TestWebApp/TestWebApp/Helper/AccountQueries.cs
@@ -25,7 +25,75 @@ namespace TestWebApp.Helper
       catch (Exception e)
       {
         Console.WriteLine(e.Message);
-        return new List<Account>(); // doubt ???
+        return new List<Account>();
+      }
+    }
+
+    public List<Account> GetAllCreatedInLastDays()
+    {
+      try
+      {
+        using (ISession session = NHibernateSession.OpenSession())
+        {
+          var accounts = session.QueryOver<Account>().Where(x => x.DateCreated >= DateTime.Today.AddDays(-1)).List<Account>();
+          return accounts.ToList();
+        }
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+        return new List<Account>();
+      }
+    }
+
+    public List<Account> GetAllUpdatedInLastDays()
+    {
+      try
+      {
+        using (ISession session = NHibernateSession.OpenSession())
+        {
+          var accounts = session.QueryOver<Account>().Where(x => x.DateUpdated >= DateTime.Today.AddDays(-1)).List<Account>();
+          return accounts.ToList();
+        }
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+        return new List<Account>();
+      }
+    }
+
+    public int GetCountOfUpdatedInLastDays()
+    {
+      try
+      {
+        using (ISession session = NHibernateSession.OpenSession())
+        {
+          var accounts = session.QueryOver<Account>().Where(x => x.DateUpdated >= DateTime.Today.AddDays(-1)).RowCount();
+          return accounts;
+        }
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+        return -1;
+      }
+    }
+
+    public int GetCountOfCreatedInLastDays()
+    {
+      try
+      {
+        using (ISession session = NHibernateSession.OpenSession())
+        {
+          var accounts = session.QueryOver<Account>().Where(x => x.DateUpdated >= DateTime.Today.AddDays(-1)).RowCount();
+          return accounts;
+        }
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+        return -1;
       }
     }
 
