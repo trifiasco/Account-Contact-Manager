4040224c907a04c92fcddd885bfbe144bb2d8c75
ENOSISBD\arnabroy <trifiasco@gmail.com>
2017-12-14 17:43:34 +0600

    Added a home view model

---
+++ TestWebApp/TestWebApp/Models/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestWebApp.Models
+{
+  public class HomeViewModel
+  {
+    public int AccountCreatedCount { get; set; }
+    public int AccountUpdatedCount { get; set; }
+    public int ContactCreatedCount { get; set; }
+  }
+}
