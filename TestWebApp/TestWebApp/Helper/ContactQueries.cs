77d28df2f9792b6d9a32e9bddd4a49a8bed324ac
ENOSISBD\arnabroy <trifiasco@gmail.com>
2017-12-14 17:55:35 +0600

    Added custom query methods to call stored procedure and updated the interface

--- TestWebApp/TestWebApp/Helper/ContactQueries.cs
+++ TestWebApp/TestWebApp/Helper/ContactQueries.cs
@@ -1,5 +1,6 @@
 ﻿using NHibernate;
 using NHibernate.Linq;
+using NHibernate.Transform;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,81 +10,115 @@ using TestWebApp.Models;
 
 namespace TestWebApp.Helper
 {
-    public class ContactQueries : IContactQueries
+  public class ContactQueries : IContactQueries
+  {
+    public List<Contact> GetAll()
     {
-        public List<Contact> GetAll()
+      try
+      {
+        using (ISession session = NHibernateSession.OpenSession())
         {
-            try
-            {
-                using (ISession session = NHibernateSession.OpenSession())
-                {
-                    var contacts = session.Query<Contact>().ToList();
-                    return contacts;
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                return new List<Contact>(); // doubt ???
-            }
+          var contacts = session.Query<Contact>().ToList();
+          return contacts;
         }
-
-        public Contact GetOneById(int id)
-        {
-            try
-            {
-                using (ISession session = NHibernateSession.OpenSession())
-                {
-                    var contact = session.Get<Contact>(id);
-                    return contact;
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                return new Contact();
-            }
-        }
-
-        public void Save(Contact contact)
-        {
-            try
-            {
-                using (ISession session = NHibernateSession.OpenSession())
-                {
-                    using (ITransaction transaction = session.BeginTransaction())
-                    {
-                        session.SaveOrUpdate(contact);
-                        transaction.Commit();
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                return;
-            }
-        }
-
-        public void Delete(Contact contact)
-        {
-            try
-            {
-                using (ISession session = NHibernateSession.OpenSession())
-                {
-                    using (ITransaction transaction = session.BeginTransaction())
-                    {
-                        session.Delete(contact);
-                        transaction.Commit();
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                return;
-            }
-        }
-
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+        return new List<Contact>(); // doubt ???
+      }
     }
+
+    public List<Contact> GetAllCreatedInLastDays()
+    {
+      try
+      {
+        using (ISession session = NHibernateSession.OpenSession())
+        {
+          var contacts = session.CreateSQLQuery("exec GetContactsCreatedInLastDays").AddEntity(typeof(Contact)).List<Contact>();
+          return contacts.ToList();
+        }
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+        return new List<Contact>(); 
+      }
+    }
+
+    public List<Contact> GetAllUpdatedInLastDays()
+    {
+      try
+      {
+        using (ISession session = NHibernateSession.OpenSession())
+        {
+          var contacts = session.CreateSQLQuery("exec GetContactsUpdatedInLastDays").AddEntity(typeof(Contact)).List<Contact>();
+          return contacts.ToList();
+        }
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+        return new List<Contact>();
+      }
+    }
+
+    public Contact GetOneById(int id)
+    {
+      try
+      {
+        using (ISession session = NHibernateSession.OpenSession())
+        {
+          var contact = session.Get<Contact>(id);
+          return contact;
+        }
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+        return new Contact();
+      }
+    }
+
+    public void Save(Contact contact)
+    {
+      try
+      {
+        using (ISession session = NHibernateSession.OpenSession())
+        {
+          using (ITransaction transaction = session.BeginTransaction())
+          {
+            session.SaveOrUpdate(contact);
+            transaction.Commit();
+          }
+        }
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+        return;
+      }
+    }
+
+    public void Delete(Contact contact)
+    {
+      try
+      {
+        using (ISession session = NHibernateSession.OpenSession())
+        {
+          using (ITransaction transaction = session.BeginTransaction())
+          {
+            session.Delete(contact);
+            transaction.Commit();
+          }
+        }
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+        return;
+      }
+    }
+
+  }
 }
--- TestWebApp/TestWebApp/Helper/IContactQueries.cs
+++ TestWebApp/TestWebApp/Helper/IContactQueries.cs
@@ -6,6 +6,8 @@ namespace TestWebApp.Helper
   public interface IContactQueries
   {
     List<Contact> GetAll();
+    List<Contact> GetAllCreatedInLastDays();
+    List<Contact> GetAllUpdatedInLastDays();
     Contact GetOneById(int id);
     void Save(Contact contact);
     void Delete(Contact contact);
