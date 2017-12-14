afacaf7c9e37dd2fcb7b594406f982825397c044
ENOSISBD\arnabroy <trifiasco@gmail.com>
2017-12-14 17:42:17 +0600

    Created view model for last 2 days created and updated accounts

---
+++ TestWebApp/TestWebApp/Models/AccountUpdatedInLastDaysViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using TestWebApp.Entity;
+
+namespace TestWebApp.Models
+{
+  public class AccountUpdatedInLastDaysViewModel
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public IList<Contact> Contacts { get; set; }
+  }
+}
