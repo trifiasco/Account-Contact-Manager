ba5e3d1472f3c5734f7cc11c3e4751d5ba471299
ENOSISBD\arnabroy <trifiasco@gmail.com>
2017-12-14 17:40:23 +0600

    Updated interface

--- TestWebApp/TestWebApp/Helper/IAccountQueries.cs
+++ TestWebApp/TestWebApp/Helper/IAccountQueries.cs
@@ -6,6 +6,10 @@ namespace TestWebApp.Helper
   public interface IAccountQueries
   {
     List<Account> GetAll();
+    List<Account> GetAllCreatedInLastDays();
+    List<Account> GetAllUpdatedInLastDays();
+    int GetCountOfCreatedInLastDays();
+    int GetCountOfUpdatedInLastDays();
     Account GetOneById(int id);
     void Save(Account account);
     void Delete(Account account);
