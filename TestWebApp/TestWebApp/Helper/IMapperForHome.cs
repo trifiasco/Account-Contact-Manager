dbe63c9dd77a9e96a13e3c13b027c7489b04113f
ENOSISBD\arnabroy <trifiasco@gmail.com>
2017-12-14 17:41:41 +0600

    Added mapper interface and class for Home

---
+++ TestWebApp/TestWebApp/Helper/IMapperForHome.cs
@@ -0,0 +1,9 @@
+﻿using TestWebApp.Models;
+
+namespace TestWebApp.Helper
+{
+  public interface IMapperForHome
+  {
+    HomeViewModel MapToHomeViewModel(int accountCreateCount, int accountUpdateCount,int contactCreatedCount);
+  }
+}
---
+++ TestWebApp/TestWebApp/Helper/MapperForHome.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TestWebApp.Models;
+
+namespace TestWebApp.Helper
+{
+  public class MapperForHome : IMapperForHome
+  {
+    public HomeViewModel MapToHomeViewModel(int accountCreateCount, int accountUpdateCount,int contactCreatedCount)
+    {
+      var homeViewModel = new HomeViewModel();
+      homeViewModel.AccountCreatedCount = accountCreateCount;
+      homeViewModel.AccountUpdatedCount = accountUpdateCount;
+      homeViewModel.ContactCreatedCount = contactCreatedCount;
+      return homeViewModel;
+    }
+  }
+}
