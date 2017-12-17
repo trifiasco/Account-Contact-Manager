411ed0fcd05b689f3d2a238ed3e4880b5063b8db
ENOSISBD\arnabroy <trifiasco@gmail.com>
2017-12-17 19:14:35 +0600

    removed added temporary api project

---
+++ TestWebApp/WebApplicationAPI/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using DomainClass.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DomainClass.QueryHelper;
+
+namespace WebApplicationAPI.Controllers
+{
+  public class AccountController : ApiController
+  {
+    public IEnumerable<Account> GetAllAccounts()
+    {
+      AccountQueries accountQueries = new AccountQueries();
+      List<Account> accounts = new List<Account>();
+      accounts = accountQueries.GetAll();
+      return accounts;
+    }
+  }
+}
---
+++ TestWebApp/WebApplicationAPI/Models/AccountViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationAPI.Models
+{
+  public class AccountViewModel
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+  }
+}
