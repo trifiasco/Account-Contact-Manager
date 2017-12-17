6d8c2fe3fb49aeed36e9cfc1bcd959f11c723dce
ENOSISBD\arnabroy <trifiasco@gmail.com>
2017-12-17 17:33:07 +0600

    refactored code

--- TestWebApp/DomainClass/NHibernateSession.cs
+++ TestWebApp/DomainClass/NHibernateSession.cs
@@ -8,16 +8,20 @@ namespace DomainClass
   {
     public static ISession OpenSession()
     {
-      var configuration = new Configuration();//.AddAssembly("DomainClass");
+      var configuration = new Configuration();//.AddAssembly("DomainClass.NHibernate");
       //var configurationPath = HttpContext.Current.Server.MapPath(@"~\DomainClass\NHibernate\hibernate.cfg.xml");
       //configuration.Configure(configurationPath);
-      configuration.Configure(@"D:\WorkSpace\TestWebApp\DomainClass\NHibernate\hibernate.cfg.xml");
+
       //var accountConfigurationFile = HttpContext.Current.Server.MapPath(@"~\DomainClass\NHibernate\Account.hbm.xml");
       //configuration.AddFile(accountConfigurationFile);
-      configuration.AddFile(@"D:\WorkSpace\TestWebApp\DomainClass\NHibernate\Account.hbm.xml");
+
       //var contactConfigurationFile = HttpContext.Current.Server.MapPath(@"~\DomainClass\NHibernate\Contact.hbm.xml");
       //configuration.AddFile(contactConfigurationFile);
+
+      configuration.Configure(@"D:\WorkSpace\TestWebApp\DomainClass\NHibernate\hibernate.cfg.xml");
+      configuration.AddFile(@"D:\WorkSpace\TestWebApp\DomainClass\NHibernate\Account.hbm.xml");
       configuration.AddFile(@"D:\WorkSpace\TestWebApp\DomainClass\NHibernate\Contact.hbm.xml");
+
       ISessionFactory sessionFactory = configuration.BuildSessionFactory();
       return sessionFactory.OpenSession();
     }
