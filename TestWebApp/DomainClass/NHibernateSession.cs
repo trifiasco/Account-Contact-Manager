957bde3dde5b15b0962c42fe2121378b4f0bff91
ENOSISBD\arnabroy <trifiasco@gmail.com>
2017-12-19 15:31:19 +0600

    Updated Nhibernate session class. Implemented singlton session factory

--- TestWebApp/DomainClass/NHibernateSession.cs
+++ TestWebApp/DomainClass/NHibernateSession.cs
@@ -1,6 +1,5 @@
 ﻿using NHibernate;
 using NHibernate.Cfg;
-using System.Web;
 
 namespace DomainClass
 {
@@ -18,22 +17,6 @@ namespace DomainClass
         _sessionFactory = configuration.BuildSessionFactory();
       }
       return _sessionFactory.OpenSession();
-      //var configuration = new Configuration();//.AddAssembly("DomainClass.NHibernate");
-      ////var configurationPath = HttpContext.Current.Server.MapPath(@"~\DomainClass\NHibernate\hibernate.cfg.xml");
-      ////configuration.Configure(configurationPath);
-
-      ////var accountConfigurationFile = HttpContext.Current.Server.MapPath(@"~\DomainClass\NHibernate\Account.hbm.xml");
-      ////configuration.AddFile(accountConfigurationFile);
-
-      ////var contactConfigurationFile = HttpContext.Current.Server.MapPath(@"~\DomainClass\NHibernate\Contact.hbm.xml");
-      ////configuration.AddFile(contactConfigurationFile);
-
-      //configuration.Configure(@"D:\WorkSpace\TestWebApp\DomainClass\NHibernate\hibernate.cfg.xml");
-      //configuration.AddFile(@"D:\WorkSpace\TestWebApp\DomainClass\NHibernate\Account.hbm.xml");
-      //configuration.AddFile(@"D:\WorkSpace\TestWebApp\DomainClass\NHibernate\Contact.hbm.xml");
-
-      //ISessionFactory sessionFactory = configuration.BuildSessionFactory();
-      //return sessionFactory.OpenSession();
     }
   }
 }
