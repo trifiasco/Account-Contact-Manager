1f13643448f288ae86a1514526315ef9cf9f6dc9
ENOSISBD\arnabroy <trifiasco@gmail.com>
2017-12-19 13:25:59 +0600

    tried to implement singleton view pattern for the class

--- TestWebApp/DomainClass/NHibernateSession.cs
+++ TestWebApp/DomainClass/NHibernateSession.cs
@@ -6,24 +6,34 @@ namespace DomainClass
 {
   public class NHibernateSession
   {
+    private static ISessionFactory _sessionFactory;
     public static ISession OpenSession()
     {
-      var configuration = new Configuration();//.AddAssembly("DomainClass.NHibernate");
-      //var configurationPath = HttpContext.Current.Server.MapPath(@"~\DomainClass\NHibernate\hibernate.cfg.xml");
-      //configuration.Configure(configurationPath);
+      if(_sessionFactory==null)
+      {
+        var configuration = new Configuration();
+        configuration.Configure(@"D:\WorkSpace\TestWebApp\DomainClass\NHibernate\hibernate.cfg.xml");
+        configuration.AddFile(@"D:\WorkSpace\TestWebApp\DomainClass\NHibernate\Account.hbm.xml");
+        configuration.AddFile(@"D:\WorkSpace\TestWebApp\DomainClass\NHibernate\Contact.hbm.xml");
+        _sessionFactory = configuration.BuildSessionFactory();
+      }
+      return _sessionFactory.OpenSession();
+      //var configuration = new Configuration();//.AddAssembly("DomainClass.NHibernate");
+      ////var configurationPath = HttpContext.Current.Server.MapPath(@"~\DomainClass\NHibernate\hibernate.cfg.xml");
+      ////configuration.Configure(configurationPath);
 
-      //var accountConfigurationFile = HttpContext.Current.Server.MapPath(@"~\DomainClass\NHibernate\Account.hbm.xml");
-      //configuration.AddFile(accountConfigurationFile);
+      ////var accountConfigurationFile = HttpContext.Current.Server.MapPath(@"~\DomainClass\NHibernate\Account.hbm.xml");
+      ////configuration.AddFile(accountConfigurationFile);
 
-      //var contactConfigurationFile = HttpContext.Current.Server.MapPath(@"~\DomainClass\NHibernate\Contact.hbm.xml");
-      //configuration.AddFile(contactConfigurationFile);
+      ////var contactConfigurationFile = HttpContext.Current.Server.MapPath(@"~\DomainClass\NHibernate\Contact.hbm.xml");
+      ////configuration.AddFile(contactConfigurationFile);
 
-      configuration.Configure(@"D:\WorkSpace\TestWebApp\DomainClass\NHibernate\hibernate.cfg.xml");
-      configuration.AddFile(@"D:\WorkSpace\TestWebApp\DomainClass\NHibernate\Account.hbm.xml");
-      configuration.AddFile(@"D:\WorkSpace\TestWebApp\DomainClass\NHibernate\Contact.hbm.xml");
+      //configuration.Configure(@"D:\WorkSpace\TestWebApp\DomainClass\NHibernate\hibernate.cfg.xml");
+      //configuration.AddFile(@"D:\WorkSpace\TestWebApp\DomainClass\NHibernate\Account.hbm.xml");
+      //configuration.AddFile(@"D:\WorkSpace\TestWebApp\DomainClass\NHibernate\Contact.hbm.xml");
 
-      ISessionFactory sessionFactory = configuration.BuildSessionFactory();
-      return sessionFactory.OpenSession();
+      //ISessionFactory sessionFactory = configuration.BuildSessionFactory();
+      //return sessionFactory.OpenSession();
     }
   }
 }
